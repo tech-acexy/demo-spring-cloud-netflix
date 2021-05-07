f5fa4fa1139075c2ab1b3a7391820b5c745d25b3
Acexy <acexy@acexy.cn>
2021-05-07 21:57:25 +0800

    删除无用的package

--- netflix-common/src/main/java/cn/acexy/tech/springcloud/feign/service/FeignCarService.java
+++ netflix-common/src/main/java/cn/acexy/tech/springcloud/feign/service/FeignCarService.java
@@ -3,10 +3,8 @@ package cn.acexy.tech.springcloud.feign.service;
 import cn.acexy.tech.springcloud.common.bean.Car;
 import cn.acexy.tech.springcloud.common.bean.User;
 import org.springframework.cloud.openfeign.SpringQueryMap;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping(value = "car")
 public interface FeignCarService {
